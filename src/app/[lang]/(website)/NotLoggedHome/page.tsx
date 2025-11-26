44edd498ac5d7a0c71bd8c85f5c743bdc728c6c0
Abdulrahim <abdulrahimsakr01@gmail.com>
2025-11-27 01:40:23 +0200

    start work on sec6

--- src/app/[lang]/(website)/NotLoggedHome/page.tsx
+++ src/app/[lang]/(website)/NotLoggedHome/page.tsx
@@ -3,6 +3,7 @@ import Sec2 from "@/components/NotLoggedHome/Sec2/Sec2";
 import Sec3 from "@/components/NotLoggedHome/Sec3/Sec3";
 import Sec4 from "@/components/NotLoggedHome/Sec4/Sec4";
 import Sec5 from "@/components/NotLoggedHome/Sec5/Sec5";
+import Sec6 from "@/components/NotLoggedHome/Sec6/Sec6";
 
 export default function NotLoggedHome() {
   return (
@@ -10,8 +11,11 @@ export default function NotLoggedHome() {
       <Sec1 />
       <Sec2 />
       <Sec3 />
-      <Sec4 />
-      <Sec5 />
+      <div>
+        <Sec4 />
+        <Sec5 />
+      </div>
+      <Sec6 />
     </div>
   );
 }
---
+++ src/components/NotLoggedHome/Sec6/Sec6.tsx
@@ -0,0 +1,20 @@
+export default function Sec6() {
+  return (
+    <div className="flex flex-col justify-center items-center gap-5 text-left md:text-center py-40 px-5 md:px-20 lg:px-48">
+      <div className="text-5xl font-bold text-center">
+        Create elaborate images,
+        <br /> without being a graphic designer
+      </div>
+      <div className="text-center">
+        It&apos;s like being a Photoshop pro, without actually spending the time
+        to learn Photoshop.
+      </div>
+      <div className="flex flex-col sm:flex-row justify-between items-center">
+        <div className="text-xl font-semibold">Set your canvas size</div>
+        <div className="text-sm">
+          You can change the dimensions up to 2048✕2048.
+        </div>
+      </div>
+    </div>
+  );
+}
